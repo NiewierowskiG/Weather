d078db8dc68df1a6374dc1c357f310fed0ea3cb6
GrzegorzNiewierowski <gniewierowski@gmail.com>
2022-05-10 16:10:59 +0200

    Map and weather api

--- Weather/views.py
+++ Weather/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 import requests
+import folium
 
 
 def home(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={key}'
-    city = "Drohiczyn"
-    api_key = 'fcaf07b0c6e6a7745e7f4a0c49bd6fd9'
-    api = requests.get(url.format(city=city, key=api_key)).json()
-    weather = {
-        "weather_desc": api['weather'][0]['description'],
-        "temp": (api['main']['temp'] - 32) * 5.0 / 9.0  # convert Fahrenheit to Celsius
-    }
-    return HttpResponse("eo")
+    if request.GET.get('submit'):
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={key}'
+        city = request.GET.get('City')
+        api_key = 'fcaf07b0c6e6a7745e7f4a0c49bd6fd9'
+        api = requests.get(url.format(city=city, key=api_key)).json()
+        try:
+            map = folium.Map(width=400, height=400, location=[api['coord']['lat'], api['coord']['lon']], zoom_start=12)
+        except KeyError:
+            return render(request, 'Weather/LoadCity.html', {'error': True})
+        map = map._repr_html_()
+        weather = {
+            "weather_desc": api['weather'][0]['description'],
+            "temp": round((api['main']['temp'] - 32) * 5.0 / 9.0, 2), # convert Fahrenheit to Celsius
+            "icon": f"http://openweathermap.org/img/w/{ api['weather'][0]['icon'] }.png",
+            "city": city
+        }
+        return render(request, 'Weather/main.html', {'map': map, 'weather': weather})
+    else:
+        return render(request, 'Weather/LoadCity.html', {'error': False})
